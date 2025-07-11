93aa61aaf59df05e916d5442b52c7e2ff3254151
Dima1202 <dimalsha0506@gmail.com>
2025-07-11 20:14:49 +0530

    Database Connection

---
+++ src/main/util/DBConnection.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3307/pahanaedu";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException("MySQL JDBC Driver not found", e);
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }
+}
+
